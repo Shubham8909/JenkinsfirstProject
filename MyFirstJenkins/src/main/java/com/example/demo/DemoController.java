80a59ea4ef3df1333e12621f9132f1a61ac17334
Shubham8909 <shubhamverma@197nodnb24482>
2021-05-09 18:56:49 +0530

    added new massage

--- MyFirstJenkins/src/main/java/com/example/demo/DemoController.java
+++ MyFirstJenkins/src/main/java/com/example/demo/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 	
 	@GetMapping("/hello")
 	public String getHello() {
-		return "Hello! Jenkins Learners.";
+		return "Hello! Jenkins Learners this is shubham.";
 	}
 
 }
